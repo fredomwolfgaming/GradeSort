8133391f012323a84fe9f36a92a807fb7f2b9291
fredomwolfgaming <113492687+fredomwolfgaming@users.noreply.github.com>
2022-09-26 09:52:43 -0500

    progress into vectors and StringStream
    i'm understanding vectors and how to use stringStream now, and almost have the tokenizer done.

--- Grade Sorter from file/Grade Sorter from file.cpp
+++ Grade Sorter from file/Grade Sorter from file.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 //ofstream = output file stream
@@ -27,29 +28,68 @@ using namespace std;
 * 
 *while getline
 * use line to pass into token convertr (getline returns true when there are more lines)
+*
+* pass by refrence (type &name)
+* update by using the object created refrencing this class s.fName[0]
+*    
 */
 
-class Students {//storage space
+class Student {//storage space
+public:
     string fName;
     string lName;
     int grade;
-    /*pass by refrence (type &name)
-    * update by using the object created refrencing this class s.fName[0]
-    */
+
 };
 
-void splitter(string currentLine)
+void splitter(string cLine)//split string into array of words, aka tokenized
 {
+    /*https://www.tutorialspoint.com/stringstream-in-cplusplus
+    * https://www.w3schools.com/cpp/cpp_strings_input.asp
+    * i should be able to access student attributes from here,
+      otherwise there needs to be get-set built into the class.
+    */
+    stringstream stStrm(cLine);//breaker class, refrence, and input string
+    vector<string> words;//variable storage for set of words that makeup the string
+    string tmp;//word storage per iteration of pulling from stream.
+
+    //store these values in student
+    while (stStrm >> tmp)
+    {//while tmp can still be accessed, one word at a time, add those tmp Words to words vector set
+        words.push_back(tmp);
+    }
+
+
+
+    string str("Hello from the dark side");
+    string tmp; // A string to store the word on each iteration.
+    stringstream str_strm(str);
+    vector<string> words; // Create vector to hold our words
+    while (str_strm >> tmp) {
+        // Provide proper checks here for tmp like if empty
+        // Also strip down symbols like !, ., ?, etc.
+        // Finally push it.
+        words.push_back(tmp);
+    }
 
 }
+
+
 
 int main()
 {
 
 
 
-    ifstream gradeFile("gradesIn.txt");
-    string currentLine;//current input from file
+    ifstream gradeFile("gradesIn.txt");//input file stream
+    string currentLine;//current input line from file
+    vector<Student> student;//create vector object, with type of student class. name it student. has 3 attributes.
+
+
+
+
+
+
     //while this is true that there is more, get from att0, and assign to variable in att1
     //while(getline(gradeFile, currentLine))
     //so grab the line, split it, assign it, then continue to next line.
